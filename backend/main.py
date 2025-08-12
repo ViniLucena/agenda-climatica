f39f32e3d17f3be62d1ae9fa7cb0d1dc28bbe04d
vinicius.cortez <vbitucortez@gmail.com>
2025-08-12 12:41:38 -0300

    adiciona tratamento de erros na requisicao da API

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query
 from backend.weather import get_weather
 
 app = FastAPI()
@@ -8,6 +8,9 @@ def root():
     return {"message": "API funcionando"}
 
 @app.get("/weather")
-def weather(city: str):
-    return get_weather(city)
+def weather(city: str = Query(..., min_length= 1, description="Nome da cidade para buscar o clima")):
+    weather_data = get_weather(city)
+    if "error" in weather_data:
+        raise HTTPException(status_code=404, detail=weather_data["error"])
+    return weather_data
     
